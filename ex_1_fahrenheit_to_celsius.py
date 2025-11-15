88ae4aa09c759925197b0f70ae04e5838895218b
Daniel555AS <daniel57orlando@gmail.com>
2025-11-15 17:43:27 +0100

    fahrenheit_to_celsius

---
+++ ex_1_fahrenheit_to_celsius.py
@@ -0,0 +1,17 @@
+# Write a Python function named fahrenheit_to_celsius that converts 
+# a given Fahrenheit temperature to Celsius, rounded to two decimal 
+# places and returns it.
+
+def fahrenheit_to_celsius(f):
+    return round((f - 32) * (5 / 9), 2)
+
+
+def main() -> None:
+    assert fahrenheit_to_celsius(98) == 36.67
+    assert fahrenheit_to_celsius(-35) == -37.22
+    assert fahrenheit_to_celsius(1337) == 725.0
+    print("All tests passed ✓")
+
+
+if __name__ == '__main__':
+    main()
